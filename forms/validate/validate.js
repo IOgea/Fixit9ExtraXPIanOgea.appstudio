c5b4b9c9dae9fb4c7b27288d44ad43c4317c68d8
IOgea <Irogea@gmail.com>
2020-10-14 21:09:29 -0500

    Completed validate and dataTable forms

---
+++ forms/validate/validate.js
@@ -0,0 +1,4 @@
+
+validate.onshow=function(){
+  alrtValid.value = "Name is a required field, Phone is required and requires a phone number, Email is required and needs a valid email, SSN is required and needs a valid SSN"
+}
