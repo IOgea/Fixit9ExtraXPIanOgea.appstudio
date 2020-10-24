15d2a350440c716ec89a2520574a2141ddc4f896
IOgea <Irogea@gmail.com>
2020-10-24 16:26:29 -0500

    fixed Validate form

--- forms/validate/validate.js
+++ forms/validate/validate.js
@@ -2,3 +2,7 @@
 validate.onshow=function(){
   alrtValid.value = "Name is a required field, Phone is required and requires a phone number, Email is required and needs a valid email, SSN is required and needs a valid SSN"
 }
+
+btnValidate.onclick=function(){
+  $("#vdtrInput").jqxValidator("validate")
+}
